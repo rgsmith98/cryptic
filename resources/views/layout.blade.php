3ae65f288c3dad9fb53fe0ad86b457935c95f8ca
Gaurav Ranpara <42828258+ranparagaurav@users.noreply.github.com>
2020-03-08 15:04:13 +0530

    Integration of Home Page Template Complete.

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>Gaurav Ranpara - Mean Stack Developer</title>
+
+        <link rel="stylesheet" href="{{ URL::asset('assets/css/plugins/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('assets/css/plugins/font-awesome.css') }}">
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/plugins/magnific-popup.css') }}">
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/plugins/simplebar.css') }}">
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/plugins/owl.carousel.min.css') }}">
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/plugins/owl.theme.default.min.css') }}">
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/plugins/jquery.animatedheadline.css') }}">
+
+		<link rel="stylesheet" href="{{ URL::asset('assets/css/style.css') }}">
+	</head>
+<body>
+	<!-- Preloader -->
+	<div id="preloader">
+		<div class="loading">
+			<div class="circle"></div>
+			<div class="text-loading">
+				<span data-text-preloader="G" class="letters">G</span>
+				<span data-text-preloader="A" class="letters">A</span>
+				<span data-text-preloader="U" class="letters">U</span>
+				<span data-text-preloader="R" class="letters">R</span>
+				<span data-text-preloader="A" class="letters">A</span>
+				<span data-text-preloader="V" class="letters">V</span>
+			</div>
+		</div>	
+	</div>
+
+
+	@yield('page_content')
+
+	<script src="{{ URL::asset('assets/js/jquery.min.js') }}"></script>
+	<script src="{{ URL::asset('assets/js/isotope.pkgd.min.js') }}"></script>
+	<script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
+	<script src="{{ URL::asset('assets/js/simplebar.js') }}"></script>
+	<script src="{{ URL::asset('assets/js/owl.carousel.min.js') }}"></script>
+	<script src="{{ URL::asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
+	<script src="{{ URL::asset('assets/js/jquery.animatedheadline.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/main.js') }}"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=false"></script>
+
+   	<!-- Page Script -->
+	<script src="{{ URL::asset('assets/js/jquery.ripples-min.js') }}"></script>
+	<script>
+		$('.home').ripples({
+			resolution: 1000,
+			dropRadius: 15,
+			perturbance: 0.02
+		});
+	</script>
+</body>
+</html>
